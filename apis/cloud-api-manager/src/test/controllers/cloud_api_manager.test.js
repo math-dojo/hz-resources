9de389d97b3a26052af7c50cd19f78909256ba90
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-06-03 21:47:26 +0100

    Add test structure for controller

---
+++ apis/cloud-api-manager/src/test/controllers/cloud_api_manager.test.js
@@ -0,0 +1,27 @@
+// jshint esversion:6
+const { describe, it } = require("mocha");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe("CloudApiManagerController", function() {
+    describe("Initialisation", function() {
+        it("should return if successful");
+        it("should throw an error if unsuccessful");
+    });
+    describe(".create", function() {
+        it("should return if successful");
+        it("should throw an error if unsuccessful");
+    });
+    describe(".update", function() {
+        it("should return if successful");
+        it("should throw an error if unsuccessful");
+    });
+    describe(".delete", function() {
+        it("should return if successful");
+        it("should throw an error if unsuccessful");
+    });    
+});
+
