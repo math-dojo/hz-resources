f85a001a589c47cd405a1c4d77e7ba8757ba38dc
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-06-04 18:19:42 +0100

    Add unit test for successful Controller creation

--- apis/cloud-api-manager/src/test/controllers/cloud_api_manager.test.js
+++ apis/cloud-api-manager/src/test/controllers/cloud_api_manager.test.js
@@ -3,13 +3,20 @@ const { describe, it } = require("mocha");
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 
+const { CloudApiManagerController } = require("../../main/controllers/cloud_api_manager");
+
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe("CloudApiManagerController", function() {
     describe("Initialisation", function() {
-        it("should return if successful");
-        it("should throw an error if unsuccessful");
+        it("should return a new CloudApiManagerController if successful", function() {
+            const returnedController = new CloudApiManagerController({provider: 'tyk', authorisation: 'fizzbuzz'});
+            expect(returnedController).to.be.a(CloudApiManagerController);
+        });
+        it("should throw an error if authorisation is null or empty");
+        it("should throw an error if the request provider is null or empty");
+        it("should throw an error if the request provider is unknown");
     });
     describe(".create", function() {
         it("should return if successful");
