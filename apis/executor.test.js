230e3d3b0b6fde44158192fa9d10c384ca180339
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-07-04 12:09:00 +0100

    Add index and executor test cases

---
+++ apis/executor.test.js
@@ -0,0 +1,11 @@
+const logger = new (require("./logging"))("executor");
+// jshint esversion:6
+const { describe, it } = require("mocha");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe("Executor Tests", function () {
+    it("should do some stuff");
+});
