7a815e467b3753d4a206fec50acd5eac7eb22734
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-07-04 13:34:24 +0100

    Validate the supplied file is js file

--- apis/index.js
+++ apis/index.js
@@ -24,6 +24,19 @@ const argv = require('yargs')
     .alias('t', 'target-branch')
     .nargs('t', 1)
     .describe('t', 'name of the branch which changes will be merged into')
+    .alias('u', 'NodeJs cli utility to execute')
+    .nargs('u', 1)
+    .describe('u', 'the path to the utilitiy\'s entrypoint file')
+    .normalize('u')
+    .coerce('u', function (suppliedarg) {
+        const status = fs.statSync(suppliedarg);
+        if (/\.js$/.test(suppliedarg) && status.isFile) {
+            return suppliedarg;
+        }
+
+        throw new Error(`the value ${suppliedarg} is not a javascript file`);
+
+    })
     .help()
-    .demandOption(['a', 'r', 's', 't'], 'Please provide both repository, source-branch and target-branch arguments to work with this tool')
+    .demandOption(['a', 'r', 's', 't', 'u'], 'Please provide all required arguments to work with this tool')
     .argv;
